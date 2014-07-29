891fcd92736458cba3ca65ee5e15f2efe260f80b
Ketul Patel <kjpatel2010@gmail.com>
2014-07-29 16:32:14 -0700

    Added the mapping algorithm and compared the run times

--- anagrams.js
+++ anagrams.js
@@ -1,4 +1,7 @@
+// test array
 var arr = ["dog", "god", "cat", "act", "tac"]
+
+// BEGIN RECURSIVE IMPLEMENTATION
 
 // helper function to check if string 1 is an anagram of string 2
 function is_anagram_of(str1, str2) {
@@ -6,14 +9,17 @@ function is_anagram_of(str1, str2) {
 	if(str1.length != str2.length) {
 		return false
 	}
-	// loop through the second word (you are only comparing the first letter of the first word)
+	// loop through the second word (you are only comparing the first 
+	// letter of the first word)
 	for(var j = 0; j < str2.length; j++) {
-		// if the letters are equal and they are the only letters in the strings return true
+		// if the letters are equal and they are the only letters in 
+		// the strings return true
 		if(str1[0] === str2[j] && str1.length === 1) {
 			return true
 		}
-		// if the letters are equal (and aren't the only letters) then take out the matching 
-		// letters and run the algorithm on the remaining strings
+		// if the letters are equal (and aren't the only letters) then 
+		// take out the matching letters and run the algorithm on the 
+		// remaining strings
 		else if(str1[0] === str2[j]) {
 			str1new = str1.slice(1)
 			str2new = str2.slice(0, j) + str2.slice(j+1)
@@ -25,11 +31,13 @@ function is_anagram_of(str1, str2) {
 		}
 	}
 }
-// anagrams function returns a js object with keys that are the first instance of the word
-// and values that are the number of anagrams 
+// anagrams function returns a js object with keys that are the first 
+// instance of the word and values that are the number of anagrams 
 function anagrams(arr) {
 	// initialize return object
 	var retobj = {}
+	var b = new Date();
+	var before = b.getTime();
 	// loop through the array
 	for(var i = 0; i < arr.length; i++) {
 		// loop through the rest of the elements 
@@ -51,6 +59,38 @@ function anagrams(arr) {
 			}
 		}
 	}
+	var a = new Date();
+	var after = a.getTime();
+	retobj.time = (+a)-(+b)
 	return retobj
 }
+
+// BEGIN HASH MAP IMPLEMENTATION
+
+function anagrams_map(arr) {
+	var b = new Date();
+	var before = b.getTime();
+	var retobj = {}
+	// loop through the array 
+	for(var i = 0; i < arr.length; i++) {
+		// sort the word alphabetically
+		var word = arr[i].split("").sort().join("")
+		// if the word is in the hash, add one to its value otherwise
+		// add it to the hash
+		if(retobj[word] != undefined) {
+			retobj[word] = retobj[word] + 1
+		} else {
+			retobj[word] = 0
+		}
+	}
+	var a = new Date();
+	var after = a.getTime();
+	retobj.time = (+a)-(+b)
+	return retobj
+}
+console.log(anagrams_map(arr))
 console.log(anagrams(arr))
+
+// each algorithm runs in <= 1 milliseconds, out of 10 run throughs
+// they showed the same time 5 times (1,1 or 0,0) and the recursive 
+// algorithm showed 0 while the mapping algorithm showed 1 5 times
